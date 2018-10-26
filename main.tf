5f9d0355ed7ef0d759efc88edb68e85b084725c4
Akhil Arra <akhilarra@mail.usf.edu>
2018-10-26 16:30:53 -0500

    new change
    testing repository

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-    region = "${var.aws_region}"
-    profile = "${var.aws_profile}"
-
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
 }
 
 # ----- IAM ------
 
 // s3_access_role
 resource "aws_iam_instance_profile" "s3_access_profile" {
-    name = "s3_access"
-    role = "${aws_iam_role.s3_access_role.name}"  
+  name = "s3_access"
+  role = "${aws_iam_role.s3_access_role.name}"
 }
 
 resource "aws_iam_role_policy" "s3_access_policy" {
@@ -31,9 +30,9 @@ EOF
 }
 
 resource "aws_iam_role" "s3_access_role" {
-    name = "s3_access_role"
+  name = "s3_access_role"
 
-    assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
     {
         "Version" : "2012-10-17",
         "Statement" : [
@@ -53,46 +52,360 @@ resource "aws_iam_role" "s3_access_role" {
 # -----VPC-----
 
 resource "aws_vpc" "wp_vpc" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    enable_dns_hostnames = true
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_hostnames = true
 
-    tags{
-        Name = "wp_vpc"
-        Owner = "Akhil"
-    }
+  tags {
+    Name  = "wp_vpc"
+    Owner = "Akhil"
+  }
 }
-
 
 # Internet Gateway
 
 resource "aws_internet_gateway" "wp_internet_gateway" {
-    vpc_id = "${aws_vpc.wp_vpc.id}"
-    tags{
-        Name = "wp_igw"
-        Owner = "Akhil"
-    }
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name  = "wp_igw"
+    Owner = "Akhil"
+  }
 }
+
 # Route Tables
 
 resource "aws_route_table" "wp_public_rt" {
-    vpc_id = "${aws_vpc.wp_vpc.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
-    }
+  vpc_id = "${aws_vpc.wp_vpc.id}"
 
-    tags{
-        Name = "wp_rt_public"
-        Owner = "Akhil"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+  }
+
+  tags {
+    Name  = "wp_rt_public"
+    Owner = "Akhil"
+  }
 }
 
 resource "aws_default_route_table" "wp_private_rt" {
-    default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
-    
-    tags{
-        Name = "wp_rt_private"
-        Owner = "Akhil"
-    }
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+    Name  = "wp_rt_private"
+    Owner = "Akhil"
+  }
 }
+
+# Subnet
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name  = "wp_sn_public1"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name  = "wp_sn_public2"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name  = "wp_sn_private1"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name  = "wp_sn_private2"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_rds1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name  = "wp_sn_rds1"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_rds2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name  = "wp_sn_rds2"
+    Owner = "Akhil"
+  }
+}
+
+resource "aws_subnet" "wp_rds3_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds3"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[2]}"
+
+  tags {
+    Name  = "wp_sn_rds3"
+    Owner = "Akhil"
+  }
+}
+
+# rds subnet group
+
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
+  name = "wp_rds_subnetgroup"
+
+  subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}",
+    "${aws_subnet.wp_rds2_subnet.id}",
+    "${aws_subnet.wp_rds3_subnet.id}",
+  ]
+
+  tags {
+    Name  = "wp_sng_rds"
+    Owner = "Akhil"
+  }
+}
+
+# subnet Associations
+
+resource "aws_route_table_association" "wp_public1_assoc" {
+  subnet_id      = "${aws_subnet.wp_public1_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_public2_assoc" {
+  subnet_id      = "${aws_subnet.wp_public2_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private1_assoc" {
+  subnet_id      = "${aws_subnet.wp_private1_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private2_assoc" {
+  subnet_id      = "${aws_subnet.wp_private2_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+# SECURITY GROUPS
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "used for access to the dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#public security group
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "used for the elastic load balancer for public access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# private security group
+
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = "Used for private instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # Access from other security groups
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#RDS Security Group
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "Used for DB instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # SQL access from public/private security group
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}",
+    ]
+  }
+}
+
+#VPC Endpoint
+
+resource "aws_vpc_endpoint" "wp_private-s3_endpoint" {
+  vpc_id       = "${aws_vpc.wp_vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+
+  route_table_ids = ["${aws_vpc.wp_vpc.main_route_table_id}",
+    "${aws_route_table.wp_public_rt.id}",
+  ]
+
+  policy = <<POLICY
+    {
+    "Statement": [
+        {
+            "Action": "*",
+            "Effect": "Allow",
+            "Resource": "*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
+
+#S3 code Bucket
+
+resource "random_id" "wp_code_bucket" {
+  byte_length = 2
+}
+
+#-----S3-----
+
+resource "aws_s3_bucket" "code" {
+  bucket        = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
+  acl           = "private"
+  force_destroy = true
+
+  tags {
+    Name  = "wp_s3"
+    Owner = "Akhil"
+  }
+}
+
+# -----RDS-----
+
+resource "aws_db_instance" "wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.27"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
+  vpc_security_group_ids = ["${aws_security_group.wp_rds_sg.id}"]
+  skip_final_snapshot    = true
+}
+
+
+
+#-----ELB-----
+
+resource "aws_elb" "wp_elb" {
+    name = "${var.domain_name}-elb"
+    subnets = ["${aws_subnet.wp_public1_subnet.id}",
+        "${aws_subnet.wp_public2_subnet.id}"
+    ]
+    security_groups = ["${aws_security_group.wp_public_sg.id}"]
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+    health_check {
+        healthy_threshold = "${var.elb_healthy_threshold}"
+        unhealthy_threshold = "${var.elb_unhealthy_threshold}"
+        timeout = "${var.elb_timeout}"
+        target = "TCP:80"
+        interval = "${var.elb_interval}"
+    }
+
+    cross_zone_load_balancing = true
+    idle_timeout = 400
+    connection_draining = true
+    connection_draining_timeout = 400
+    tags {
+    Name  = "wp_elb"
+    Owner = "Akhil"
+    }  
+}
+
--- variables.tf
+++ variables.tf
@@ -5,3 +5,29 @@ variable "aws_profile" {}
 data "aws_availability_zones" "available" {}
 
 variable "vpc_cidr" {}
+
+variable "cidrs" {
+  type = "map"
+}
+
+variable "localip" {}
+
+variable "domain_name" {}
+
+variable "db_instance_class" {}
+
+variable "dbname" {}
+
+variable "dbuser" {}
+
+variable "dbpassword" {}
+
+variable "elb_healthy_threshold" {}
+
+variable "elb_unhealthy_threshold" {}
+
+variable "elb_timeout" {}
+
+variable "elb_interval" {}
+
+
